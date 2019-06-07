19fc345d71f58987eb126515b121b84a8230d32b
Sudhanshu <sudhanshu@Sudhanshus-MacBook-Air.local>
2019-06-08 00:24:14 +0530

    more functionality and UI

--- PlaygroundSwiftUI.playground/Pages/List.xcplaygroundpage/Contents.swift
+++ PlaygroundSwiftUI.playground/Pages/List.xcplaygroundpage/Contents.swift
@@ -7,16 +7,12 @@ import PlaygroundSupport
 struct ListItemModel: Identifiable {
     var id: Int
     var name: String
+    var imageName: String
 }
 
 struct MyContentView: View {
     @State var listItems: [ListItemModel] = [
-        ListItemModel(id: 0, name: "item 1"),
-        ListItemModel(id: 1, name: "item 2"),
-        ListItemModel(id: 2, name: "item 3"),
-        ListItemModel(id: 3, name: "item 4"),
-        ListItemModel(id: 4, name: "item 5"),
-        ListItemModel(id: 5, name: "item 6")
+        ListItemModel(id: 0, name: "Sudhanshu 1", imageName: "mypic.png")
     ]
     
     var body: some View {
@@ -30,7 +26,7 @@ struct MyContentView: View {
                 .navigationBarTitle(Text("Sudhanshu"))
                 .navigationBarItems(trailing: Button(action: {
                     let count = self.listItems.count + 1
-                    let item = ListItemModel(id: count, name: "item \(count)")
+                    let item = ListItemModel(id: count, name: "Sudhanshu \(count)", imageName: "mypic.png")
                     self.listItems.append(item)
                 }, label: {
                     Text("Add")
@@ -39,32 +35,67 @@ struct MyContentView: View {
     }
 }
 
+// Mark: - ItemRow
 struct ItemRow: View {
     var item: ListItemModel
     
     var body: some View {
         HStack {
-            Image(uiImage: UIImage(named: "mypic.png")!)
-                .resizable()
-                .aspectRatio(1/1, contentMode: .fit)
-                .clipShape(Circle())
+            CircularImage(name: item.imageName).frame(width: 50, height: 50, alignment: .init(horizontal: .leading, vertical: .top))
+            Text(item.name)
+            Spacer()
+            }
+            .padding(10)
+    }
+}
+
+// Mark: - DetailView
+struct DetailView: View {
+    var item: ListItemModel
+    let imageHeight: CGFloat = 100
+    
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            CircularImage(name: item.imageName).frame(width: imageHeight, height: imageHeight, alignment: .init(horizontal: .leading, vertical: .top))
+                .offset(y: -(imageHeight/2))
+                .padding(.bottom, -(imageHeight/2))
             Text(item.name)
             Spacer()
         }
     }
 }
 
-struct DetailView: View {
-    var item: ListItemModel
+import MapKit
+// Mark: - MapView
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+// Mark: - CircularImage
+struct CircularImage: View {
+    var name: String
     
     var body: some View {
-        HStack {
-            Image(uiImage: UIImage(named: "mypic.png")!)
-                .resizable()
-                .aspectRatio(1/1, contentMode: .fit)
-            Text(item.name)
-            Spacer()
-        }
+        Image(uiImage: UIImage(named: name)!)
+            .resizable()
+            .aspectRatio(1/1, contentMode: .fit)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            .shadow(radius: 5)
+        
     }
 }
 
